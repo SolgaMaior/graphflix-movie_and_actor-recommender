31409e4d316efcefd94f2656316a9c45030db71e
SolgaMaior <kylesabado05@gmail.com>
2026-08-28 03:11:58 +0800

    force design in prod

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Services\GraphService;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->isProduction()) {
+            URL::forceScheme('https');
+        }
     }
 }
